d5632f3e2a4a754e7e139074f4679638a83f57c0
陈云峰 <yunfeng.chen@luckincoffee.com>
2020-05-16 11:16:05 +0800

    js小数位数问题

--- 00-introduce-1-reduce-ts/index.js
+++ 00-introduce-1-reduce-ts/index.js
@@ -1,5 +1,5 @@
 "use strict";
-var shopCar = [1.234, 2.584, 3.451, 4.462];
+var shopCar = [1.234, 2.584, 3.451, 4.462, 5.022222];
 var shopCarSum = shopCar.reduce(function (prev, curr) {
     return (Number(prev.toFixed(2))) + (Number(curr.toFixed(2)));
 }, 0);
--- 00-introduce-1-reduce-ts/index.ts
+++ 00-introduce-1-reduce-ts/index.ts
@@ -1,4 +1,4 @@
-let shopCar: Array<number> = [1.234, 2.584, 3.451, 4.462];
+let shopCar: Array<number> = [1.234, 2.584, 3.451, 4.462, 5.022222];
 
 let shopCarSum: number = shopCar.reduce((prev: number, curr: number): number => {
   return (Number(prev.toFixed(2))) + (Number(curr.toFixed(2)));
